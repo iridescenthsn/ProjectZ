7db73b87458693627b9b263f72a604c266d94887
iridescenthsn <hassanfazlju@gmail.com>
2022-06-15 21:22:32 +0430

    +

---
+++ Source/ProjectZ/WeaponSkeletalMeshComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "WeaponSkeletalMeshComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTZ_API UWeaponSkeletalMeshComponent : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+	
+};
